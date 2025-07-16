643c935c2b8595bb62a866df9778b4ebef7237f9
Andrei Makarov <aomakarov04@gmail.com>
2025-07-16 18:51:17 -0400

    Out dir & main.c

---
+++ src/main.c
@@ -0,0 +1,12 @@
+// File:    main.c
+// Author:  Andrei Makarov
+// Desc:    The main file
+// //
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello world!\n");
+    return 0;
+}
