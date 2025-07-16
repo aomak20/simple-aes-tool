ec30b7c6ec223ac37e5dd0b747871d53a85c5101
Andrei Makarov <aomakarov04@gmail.com>
2025-07-16 18:51:18 -0400

    More flags/validation

--- src/main.c
+++ src/main.c
@@ -20,7 +20,19 @@ void print_error(char err_msg[])
 
 void print_usage()
 {
-    printf("Help message\n");
+    printf("saesu: Simple AES Utility\n\n");
+
+    printf("A simple AES program that runs in EBC mode. Write more stuff here later.\n\n");
+
+    printf("-d, --decrypt\tDecryption mode\n");
+    printf("-e, --encrypt\tEncryption mode\n");
+    printf("-f, --force\tForce output file overwrite\n");
+    printf("-i, --input\tInput file path\n");
+    printf("-o, --output\tOutput file path\n");
+    printf("-k, --key\tKey file path\n\n");
+
+    printf("Example usage:\n");
+    printf("saesu [-d/-e] -i input_file [-o output_file] -k key_file [-f]\n");
 }
 
 void print_version()
@@ -39,6 +51,7 @@ int main(int argc, char* argv[])
     // Flags and vars
     bool encrypt_flag = false;
     bool decrypt_flag = false;
+    bool force_flag = false;
     char* input_path = NULL;
     char* output_path = NULL;
     char* key_path = NULL;
@@ -60,40 +73,44 @@ int main(int argc, char* argv[])
 
     for(int i = 1; i < argc; i++)
     {
-        if(strcmp(argv[i], "-in") == 0)
+        if(strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--input") == 0)
         {
             if(i == argc - 1)
             {
-                print_error("Expected more input");
+                print_error("Expected input file path");
                 return 0;
             }
             input_path = argv[++i];
         }
-        else if(strcmp(argv[i], "-out") == 0)
+        else if(strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0)
         {
             if(i == argc - 1)
             {
-                print_error("Expected more input");
+                print_error("Expected output file path");
                 return 0;
             }
             output_path = argv[++i];
         }
-        else if(strcmp(argv[i], "-key") == 0)
+        else if(strcmp(argv[i], "-k") == 0 || strcmp(argv[i], "--key") == 0)
         {
             if(i == argc - 1)
             {
-                print_error("Expected more input");
+                print_error("Expected key file path");
                 return 0;
             }
             key_path = argv[++i];
         }
-        else if(strcmp(argv[i], "-e") == 0)
+        else if(strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--encrypt") == 0)
         {
             encrypt_flag = true;
         }
-        else if(strcmp(argv[i], "-d") == 0)
+        else if(strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--decrypt") == 0)
         {
             decrypt_flag = true;
+        }
+        else if(strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--force") == 0)
+        {
+            force_flag = true;
         }
         else{
             int err_msg_len = strlen("Unexpected flag \"") + strlen(argv[i]) + 1;
@@ -114,24 +131,40 @@ int main(int argc, char* argv[])
     }
     if(input_path == NULL)
     {
-        print_error("Must specify input path (-in).\n");
+        print_error("Must specify input path (-i).\n");
         return 0;
     }
     if(key_path == NULL)
     {
-        print_error("Must specify key path (-key).\n");
+        print_error("Must specify key path (-k).\n");
         return 0;
     }
+    if(output_path != NULL)
+    {
+        if(strcmp(input_path, output_path) == 0)
+        {
+            print_error("Output file cannot be same as input file.\n");
+            return 0;
+        }
+        if(strcmp(key_path, output_path) == 0)
+        {
+            print_error("Output file cannot be same as key file.\n");
+            return 0;
+        }
+    }
+
+    // Allocate string for a possible newly constructed output_path
+    int new_output_len = strlen(input_path) + strlen("-xxcrypted");
+
+    char* input_name;
+    char* input_ext;
+    input_name = strtok(input_path, ".");
+    input_ext = strtok(NULL, "");
+
+    char new_output_path[new_output_len];
+
     if(output_path == NULL)
     {
-        int new_output_len = strlen(input_path) + strlen("-xxcrypted");
-
-        char* input_name;
-        char* input_ext;
-        input_name = strtok(input_path, ".");
-        input_ext = strtok(NULL, "");
-
-        char new_output_path[new_output_len];
         strcpy(new_output_path, input_name);
         if(encrypt_flag)
         {
@@ -147,7 +180,6 @@ int main(int argc, char* argv[])
         }
 
         output_path = new_output_path;
-        // TODO: This works, but once you leave scope of this if, new_output_path is freed and becomes nonsense
     }
 
     // Debug prints
